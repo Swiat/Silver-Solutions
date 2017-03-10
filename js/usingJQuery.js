16080388ec67226d6a0c66e509b0add3b65bbc78
Swiat <ksavisw@mail.ru>
2017-03-10 17:32:42 +0200

    Revert "New"
    This reverts commit 798534a51f8edd827a535c1c5c2c0e7fb3d30e77.

--- js/usingJQuery.js
+++
@@ -1,9 +0,0 @@
-	jQuery(document).ready(function($){
-$('.content-text-link').click(function(){
-	$('.second').fadeTo(200,1);
-	$('.third').fadeTo(200,1);
-	$('.fourth').fadeTo(200,1);
-	$('.fifth').fadeTo(200,1);
-	return false;
- });
-});
